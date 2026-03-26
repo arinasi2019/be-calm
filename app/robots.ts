04e8535ec5959570790f380857f3cc4976e173c5
RYOGEN RYU <liam@RYOGENnoMacBook-Air.local>
2026-03-26 12:40:47 +0900

    add category country city filters

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,25 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://becalm.social";
+
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://becalm.social/sitemap.xml",
-    host: "https://becalm.social",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: [
+          "/api/",
+          "/_next/",
+          "/login",
+          "/profile",
+        ],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
